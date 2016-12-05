7a2fda7f03c869514769d9ebb3e96048b5f9209b
Timothy Cleveland <tjcp2@k12albemarle.org>
2016-12-04 21:40:45 -0500

    stuff

--- flask_sqlalchemy/__init__.py
+++ flask_sqlalchemy/__init__.py
@@ -810,7 +810,7 @@ class SQLAlchemy(object):
         track_modifications = app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', True)
 
         if track_modifications is None:
-            warnings.warn(FSADeprecationWarning('SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be disabled by default in the future.  Set it to True or False to suppress this warning.'))
+            warnings.warn(FSADeprecationWarning('SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be disabled by default in the future.  Set it to True or False to suppress this warning. This has been edited.’))
 
         if not hasattr(app, 'extensions'):
             app.extensions = {}
